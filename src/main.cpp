7b527e0749fd294c4437b2edf520784a940e97b7
dcvz <david@dcvz.io>
2024-08-12 12:46:03 +0200

    Update template for MM

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <librecomp/recomp.h>
 
-void your_method_here() {
+void generate() {
   fprintf(stderr, "Hello, World!\n");
 }
 
-void recomp_main(uint8_t* rdram, recomp_context* ctx) {
-  your_method_here();
+extern "C" void recomp_main(uint8_t* rdram, recomp_context* ctx) {
+  generate();
 }
